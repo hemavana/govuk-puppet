c9abefe2fba11b9f889641de4ab36d7125b0ef80
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2021-04-21 15:32:54 +0100

    Remove mongodb monitoring
    Theory is that this is having an impact on the cluster

--- modules/mongodb/manifests/monitoring.pp
+++ modules/mongodb/manifests/monitoring.pp
@@ -18,60 +18,60 @@ class mongodb::monitoring (
   # git subtree pull --prefix=modules/mongodb/files/nagios-plugin-mongodb \
   #   https://github.com/mzupan/nagios-plugin-mongodb.git \
   #   -m 'update to latest nagios-plugin-mongodb from github' master --squash
-  @icinga::plugin { 'check_mongodb.py':
-    source  => 'puppet:///modules/mongodb/nagios-plugin-mongodb/check_mongodb.py',
-  }
+  # @icinga::plugin { 'check_mongodb.py':
+  #   source  => 'puppet:///modules/mongodb/nagios-plugin-mongodb/check_mongodb.py',
+  # }
+  #
+  # @icinga::nrpe_config { 'check_mongo':
+  #   source  => 'puppet:///modules/mongodb/nrpe_check_mongo.cfg',
+  # }
+  #
+  # @icinga::plugin { 'check_dir_empty':
+  #   source  => 'puppet:///modules/mongodb/nagios_check_dir_empty',
+  # }
+  #
+  # @icinga::nrpe_config { 'check_dir_empty':
+  #   source  => 'puppet:///modules/mongodb/nrpe_check_dir_empty.cfg',
+  # }
 
-  @icinga::nrpe_config { 'check_mongo':
-    source  => 'puppet:///modules/mongodb/nrpe_check_mongo.cfg',
-  }
-
-  @icinga::plugin { 'check_dir_empty':
-    source  => 'puppet:///modules/mongodb/nagios_check_dir_empty',
-  }
-
-  @icinga::nrpe_config { 'check_dir_empty':
-    source  => 'puppet:///modules/mongodb/nrpe_check_dir_empty.cfg',
-  }
-
-  @@icinga::check { "check_mongod_running_${::hostname}":
-    check_command       => 'check_nrpe!check_proc_running!mongod',
-    service_description => 'mongod not running',
-    host_name           => $::fqdn,
-    notes_url           => monitoring_docs_url(check-process-running),
-  }
-
-  @@icinga::check { "check_mongod_responds_${::hostname}":
-    check_command       => 'check_nrpe!check_mongodb!connect 2 4',
-    service_description => 'mongod not responding',
-    host_name           => $::fqdn,
-    notes_url           => monitoring_docs_url(check-process-running),
-  }
-
-  @@icinga::check { "check_mongod_rollbacks_${::hostname}":
-    check_command       => "check_nrpe!check_dir_empty!${dbpath}/rollback",
-    service_description => 'mongod rollback dir should be nonexistent',
-    host_name           => $::fqdn,
-    notes_url           => monitoring_docs_url(mongodb-rollback),
-  }
-
-  @@icinga::check { "check_mongod_replication_lag_${::hostname}":
-    check_command       => 'check_nrpe!check_mongodb!replication_lag 150 300',
-    service_description => 'mongod replication lag',
-    host_name           => $::fqdn,
-    notes_url           => monitoring_docs_url(mongod-replication-lag),
-  }
-
-  @@icinga::check { "check_mongod_connections_${::hostname}":
-    check_command       =>'check_nrpe!check_mongodb!connections 80 90',
-    service_description => 'mongod percentage connection usage',
-    host_name           => $::fqdn,
-  }
-
-  @@icinga::check { "check_mongod_replset_state_${::hostname}":
-    check_command       => 'check_nrpe!check_mongodb!replset_state',
-    service_description => 'mongod replset state',
-    host_name           => $::fqdn,
-    notes_url           => monitoring_docs_url(recreate-a-mongo-instance-in-aws),
-  }
+  # @@icinga::check { "check_mongod_running_${::hostname}":
+  #   check_command       => 'check_nrpe!check_proc_running!mongod',
+  #   service_description => 'mongod not running',
+  #   host_name           => $::fqdn,
+  #   notes_url           => monitoring_docs_url(check-process-running),
+  # }
+  #
+  # @@icinga::check { "check_mongod_responds_${::hostname}":
+  #   check_command       => 'check_nrpe!check_mongodb!connect 2 4',
+  #   service_description => 'mongod not responding',
+  #   host_name           => $::fqdn,
+  #   notes_url           => monitoring_docs_url(check-process-running),
+  # }
+  #
+  # @@icinga::check { "check_mongod_rollbacks_${::hostname}":
+  #   check_command       => "check_nrpe!check_dir_empty!${dbpath}/rollback",
+  #   service_description => 'mongod rollback dir should be nonexistent',
+  #   host_name           => $::fqdn,
+  #   notes_url           => monitoring_docs_url(mongodb-rollback),
+  # }
+  #
+  # @@icinga::check { "check_mongod_replication_lag_${::hostname}":
+  #   check_command       => 'check_nrpe!check_mongodb!replication_lag 150 300',
+  #   service_description => 'mongod replication lag',
+  #   host_name           => $::fqdn,
+  #   notes_url           => monitoring_docs_url(mongod-replication-lag),
+  # }
+  #
+  # @@icinga::check { "check_mongod_connections_${::hostname}":
+  #   check_command       =>'check_nrpe!check_mongodb!connections 80 90',
+  #   service_description => 'mongod percentage connection usage',
+  #   host_name           => $::fqdn,
+  # }
+  #
+  # @@icinga::check { "check_mongod_replset_state_${::hostname}":
+  #   check_command       => 'check_nrpe!check_mongodb!replset_state',
+  #   service_description => 'mongod replset state',
+  #   host_name           => $::fqdn,
+  #   notes_url           => monitoring_docs_url(recreate-a-mongo-instance-in-aws),
+  # }
 }
