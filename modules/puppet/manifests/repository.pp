9446769fef8d3eddbec2c52b65a8c716d276137a
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2021-07-14 12:44:22 +0100

    Update gpg key for apt.puppetlabs.com

--- modules/puppet/manifests/repository.pp
+++ modules/puppet/manifests/repository.pp
@@ -41,7 +41,7 @@ class puppet::repository(
       location => 'http://apt.puppetlabs.com',
       release  => $::lsbdistcodename,
       repos    => 'main dependencies',
-      key      => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30', # Puppet Labs Release Key (Puppet Labs Release Key) <info@puppetlabs.com>
+      key      => 'D6811ED3ADEEB8441AF5AA8F4528B6CD9E61EF26', # Puppet Labs Release Key (Puppet Labs Release Key) <info@puppetlabs.com>
     }
   }
 }
