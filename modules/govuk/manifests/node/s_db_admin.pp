a781a73a13670e118df45f1107a89d3b613e07d0
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-12-13 09:06:09 +0000

    DRY up the app-specific DB admin box configuration
    We're about to create many app-specific DB admin boxes, all of
    which will have the same configuration on the node itself, so it
    makes sense to encapsulate this in one place.
    
    I did consider leaving the existing db_admin node untouched, as
    we will eventually remove the Postgres code from it when every
    Postgres-powered app is using its own RDS instance. However, it
    uses the same code we've just encapsulated (albeit in a slightly
    different order), so there's little harm in making this optimisation
    now.
    I did have to move the "include all PostgreSQL classes that create
    databases and users" declaration higher up so that all Postgres
    config was in the same place, and I avoided putting the Postgres
    config at the end of the file in case this class relies on these
    lines being at the beginning (which are now pulled in via the class).
    ```
      include govuk_env_sync
      include ::govuk::node::s_base
    ```

--- modules/govuk/manifests/node/s_db_admin.pp
+++ modules/govuk/manifests/node/s_db_admin.pp
@@ -26,8 +26,28 @@ class govuk::node::s_db_admin(
   $postgres_password    = undef,
   $postgres_port        = '5432',
 ) {
-  include ::govuk::node::s_base
-  include govuk_env_sync
+  # include the common config required for our DB admin class
+  class { '::govuk::nodes::s_postgresql_db_admin_base':
+    postgres_host       => $postgres_host,
+    postgres_user       => $postgres_user,
+    postgres_password   => $postgres_password,
+    postgres_port       => $postgres_port,
+    apt_mirror_hostname => $apt_mirror_hostname,
+  }
+
+  # include all PostgreSQL classes that create databases and users
+  -> class { '::govuk::apps::account_api::db': }
+  -> class { '::govuk::apps::content_data_admin::db': }
+  -> class { '::govuk::apps::content_publisher::db': }
+  -> class { '::govuk::apps::content_tagger::db': }
+  -> class { '::govuk::apps::email_alert_api::db': }
+  -> class { '::govuk::apps::link_checker_api::db': }
+  -> class { '::govuk::apps::local_links_manager::db': }
+  -> class { '::govuk::apps::publishing_api::db': }
+  -> class { '::govuk::apps::service_manual_publisher::db': }
+  -> class { '::govuk::apps::support_api::db': }
+
+  ### MongoDB ###
 
   apt::source { 'mongodb41':
     ensure       => 'absent',
@@ -74,54 +94,4 @@ class govuk::node::s_db_admin(
   -> class { '::govuk::apps::whitehall::db': }
 
   $mysql_backup_desc = 'RDS MySQL backup to S3'
-
-  ### PostgreSQL ###
-
-  $default_connect_settings = {
-    'PGUSER'     => $postgres_user,
-    'PGPASSWORD' => $postgres_password,
-    'PGHOST'     => $postgres_host,
-    'PGPORT'     => $postgres_port,
-  }
-
-  # To manage remote databases using the puppetlabs-postgresql module we require
-  # a local PostgreSQL server instance to be installed
-  apt::source { 'postgresql':
-    ensure       => present,
-    location     => "http://${apt_mirror_hostname}/postgresql",
-    release      => "${::lsbdistcodename}-pgdg",
-    architecture => $::architecture,
-    key          => 'B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8',
-  }
-
-  -> class { '::postgresql::server':
-    default_connect_settings => $default_connect_settings,
-  }
-
-  # This allows easy administration of the PostgreSQL backend:
-  # https://www.postgresql.org/docs/9.3/static/libpq-pgpass.html
-  -> file { '/root/.pgpass':
-    ensure  => present,
-    mode    => '0600',
-    content => "${postgres_host}:5432:*:${postgres_user}:${postgres_password}",
-  }
-
-  # This class collects the resources that are exported by the
-  # govuk_postgresql::server::db defined type
-  include ::govuk_postgresql::server::not_slave
-
-  # Ensure the client class is installed
-  class { '::govuk_postgresql::client': }
-
-  # include all PostgreSQL classes that create databases and users
-  -> class { '::govuk::apps::account_api::db': }
-  -> class { '::govuk::apps::content_data_admin::db': }
-  -> class { '::govuk::apps::content_publisher::db': }
-  -> class { '::govuk::apps::content_tagger::db': }
-  -> class { '::govuk::apps::email_alert_api::db': }
-  -> class { '::govuk::apps::link_checker_api::db': }
-  -> class { '::govuk::apps::local_links_manager::db': }
-  -> class { '::govuk::apps::publishing_api::db': }
-  -> class { '::govuk::apps::service_manual_publisher::db': }
-  -> class { '::govuk::apps::support_api::db': }
 }
